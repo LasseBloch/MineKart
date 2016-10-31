5cb8324d58c61043b7a6638f0e56b5e9ca975464
Michael Rahlff <post@m-rahlff.dk>
2016-10-31 19:16:59 +0100

    Added cmd to server script

--- mineKart.py
+++ mineKart.py
@@ -34,7 +34,7 @@ class McPictureDrawer:
 		(35,13, 53,71,27),
 		(35,14, 151,52,49),
 		(35,15, 26,22,22),
-                (159,0,210,178,161), # ID: 159 = Clay
+        (159,0,210,178,161), # ID: 159 = Clay
 		(159,1,162,84,38),
 		(159,2,150,88,109),
 		(159,3,113,109,138),
@@ -50,10 +50,10 @@ class McPictureDrawer:
 		(159,13,76,83,42),
 		(159,14,143,61,47),
 		(159,15,37,23,16),
-                (155,0,232,228,220), # ID: 155 = Quartz
-                (152,0,164,26,9), # ID: 152 = Redstone
-                (41,0,250,239,80), # ID: Gold block
-                (173,0,19,19,19) ) # ID: Coal Block
+        (155,0,232,228,220), # ID: 155 = Quartz
+        (152,0,164,26,9), # ID: 152 = Redstone
+        (41,0,250,239,80), # ID: Gold block
+        (173,0,19,19,19) ) # ID: Coal Block
 
     def __init__(self, mc_):
         self.mc = mc_
@@ -96,12 +96,9 @@ class McPictureDrawer:
 def main():
 	port = 2253
 	pos = mc.player.getTilePos()
-        mc.postToChat("pos x {}".format(pos.x))
-        mc.postToChat("pos y {}".format(pos.y))
-        mc.postToChat("pos z {}".format(pos.z))        
-        pos.x = -10
-        pos.y = 0
-        pos.z = -10
+    pos.x = -10
+    pos.y = 0
+    pos.z = -10
         
 	width = 80
 	height = 60
@@ -110,44 +107,85 @@ def main():
 	dut.setPicturePosition(pos.x + (width/2), pos.y, pos.z + 30)
 
 
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_address = ('', port)
-        sock.bind(server_address)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ('', port)
+    sock.bind(server_address)
+    while True:
+        # wait for connection
+        mc.postToChat("Waiting for connection")
+        sock.listen(1)
+        connection, client_address = sock.accept()
+        mc.postToChat("READY TO ROLL!!!")
         while True:
-            # wait for connection
-            print "Waiting for connection"
-            sock.listen(1)
-            connection, client_address = sock.accept()
-            print client_address, "Got connection"
-            while True:
-                connection.sendall('1')
-                data = connection.recv(8)
-                assembled_size_metadata = struct.unpack('I I', data[0:8])
-                print "ass size: ", assembled_size_metadata
-                assembled_image_size = assembled_size_metadata[0] * assembled_size_metadata[1] * 3
-                # try to read the rest of the picture
-                while len(data) < assembled_image_size:
-                    data += connection.recv(1024)
-                res = unpack_image(data)
 
-                dut.drawCompletePicture(res)
-                #mc.postToChat("Updated")
+            car_cmd = "00"
+            engine_on = False
+
+            # get latest command
+            with open(LOG_FILE) as fd:
+                log_data = fd.read()
+                # Strip all but latest 20 char
+                latest = log_data[ len(log_data) - 10: ].lower()
+
+                # Search for commands
+                forward_idx = latest.find("#forward")
+                reverse_idx = latest.find("#reverse")
+                right_idx = latest.find("###right")
+                left_idx = latest.find("####left")
+                key_idx = latest.find("#####key")
+
+                if( forward_idx >= 0 and reverse_idx < 0 and right_idx < 0 and left_idx < 0 key_idx < 0 ):
+                    if( engine_on ):
+                        car_cmd = "FF"
+                    else:
+                        car_cmd = "00"
+                elif( forward_idx < 0 and reverse_idx >= 0 and right_idx < 0 and left_idx < 0 key_idx < 0 ):
+                    if( engine_on ):
+                        car_cmd = "BB"
+                    else:
+                        car_cmd = "00"
+                elif( forward_idx < 0 and reverse_idx < 0 and right_idx >= 0 and left_idx < 0 key_idx < 0 ):
+                    if( engine_on ):
+                        car_cmd = "0F"
+                    else:
+                        car_cmd = "00"
+                elif( forward_idx < 0 and reverse_idx < 0 and right_idx < 0 and left_idx >= 0 key_idx < 0 ):
+                    if( engine_on ):
+                        car_cmd = "F0"
+                    else:
+                        car_cmd = "00"
+                elif( forward_idx < 0 and reverse_idx < 0 and right_idx < 0 and left_idx < 0 key_idx >= 0 ):
+                    car_cmd = "00"
+                    if( engine_on ):
+                        mc.postToChat("Engine off")
+                        engine_on = False
+                    else:
+                        engine_on = True
+                        mc.postToChat("Engine on")
+
+            connection.sendall(car_cmd)
+            data = connection.recv(8)
+            assembled_size_metadata = struct.unpack('I I', data[0:8])
+
+            assembled_image_size = assembled_size_metadata[0] * assembled_size_metadata[1] * 3
+            # try to read the rest of the picture
+            while len(data) < assembled_image_size:
+                data += connection.recv(1024)
+            res = unpack_image(data)
+
+            dut.drawCompletePicture(res)
+
 
 def unpack_image(packed_image):
-        assembled_size_metadata = struct.unpack('I I', packed_image[0:8])
-        assembled_image_size = assembled_size_metadata[0] * assembled_size_metadata[1] * 3
+    assembled_size_metadata = struct.unpack('I I', packed_image[0:8])
+    assembled_image_size = assembled_size_metadata[0] * assembled_size_metadata[1] * 3
 
-        convertedBack = struct.unpack('B'*assembled_image_size, packed_image[8:])
+    convertedBack = struct.unpack('B'*assembled_image_size, packed_image[8:])
 
-        testnp = np.asarray(convertedBack, dtype=np.uint8)
+    testnp = np.asarray(convertedBack, dtype=np.uint8)
 
-        assembledImg = np.reshape(testnp, (assembled_size_metadata[1], assembled_size_metadata[0], 3))
+    assembledImg = np.reshape(testnp, (assembled_size_metadata[1], assembled_size_metadata[0], 3))
 	return assembledImg
 
 if __name__ == "__main__":
    main()
-
-
-#img = cv2.imread("foo.jpg")
-#res = cv2.resize(img, (width, height), interpolation=cv2.INTER_AREA)
-
